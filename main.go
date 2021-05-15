bd170648c16fd4973732eaac4ae993d8d2c9c6e1
arkady <arkady>
2021-05-16 01:01:11 +0300

    debug

--- main.go
+++ main.go
@@ -185,7 +185,7 @@ func (el *eventListener) requestWillBeSent(r *network.EventRequestWillBeSent) {
 	}
 
 	if request, ok := el.requests[requestID]; ok {
-		log.Printf("Request %s already is in the map for url %s, %v", url, documentURL, r)
+		log.Printf("Request %s already is in the map for url %s: request=%v, event=%v", url, documentURL, request, r)
 	}
 	el.requests[requestID] = &Request{
 		URL:       r.Request.URL,
