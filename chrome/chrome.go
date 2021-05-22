5ec6b34bc24585688a6259a590ba1e1261e34883
arkady <arkady>
2021-05-22 21:57:09 +0300

    tune news web site criteria

--- chrome/chrome.go
+++ chrome/chrome.go
@@ -409,13 +409,9 @@ func (wpm webPageMetrics) getMetric(metric network.ResourceType) int {
 // https://pkg.go.dev/github.com/chromedp/cdproto/network#ResourceType
 // https://web.eecs.umich.edu/~harshavm/papers/imc11.pdf
 func (wpm webPageMetrics) isNewsSite() bool {
-	return wpm.getMetric(network.ResourceTypeMedia) > 1 &&
-		wpm.getMetric(network.ResourceTypeImage) > 10 &&
-		wpm.getMetric(network.ResourceTypeFont) > 5 &&
-		wpm.getMetric(network.ResourceTypeStylesheet) > 2 &&
-		wpm.getMetric(network.ResourceTypeScript) > 10 &&
-		wpm.getMetric(network.ResourceTypeXHR) > 5 &&
-		wpm.getMetric(network.ResourceTypeDocument) > 2 &&
+	return wpm.getMetric(network.ResourceTypeImage) > 40 &&
+		wpm.getMetric(network.ResourceTypeFont) > 3 &&
+		wpm.getMetric(network.ResourceTypeScript) > 20 &&
 		wpm.getMetric("ad") > 4 &&
 		len(wpm.dependencies) > 10
 }
